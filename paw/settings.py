f300cd3593691827328fd8b363399225837cee90
Avish Malik <69065857+avishmalik@users.noreply.github.com>
2024-05-19 11:21:05 +0000

    fixed whitenoise issue

--- paw/settings.py
+++ paw/settings.py
@@ -153,7 +153,12 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "paw" / "staticfiles"
-STATICFILES_STORAGE='whitenoise.storage.CompressedStaticFilesStorage'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+# STATICFILES_STORAGE='whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "paw" / "media"
