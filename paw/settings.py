c812a2cd8d73c0ef7daa4368c3e6b21c4ea3c95f
Avish Malik <69065857+avishmalik@users.noreply.github.com>
2024-05-19 11:44:29 +0000

    set static_dir

--- paw/settings.py
+++ paw/settings.py
@@ -147,12 +147,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-# STATICFILES_DIRS = [
-#     BASE_DIR  / "static",
-# ]
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "paw" / "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR  / "static",
+]
 # STORAGES = {
 #     "staticfiles": {
 #         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
